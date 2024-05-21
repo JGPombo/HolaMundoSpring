ff98a920de7e70011801ed5de7e2499088f56f48
javi <javi3gp@gmail.com>
2024-05-22 00:13:49 +0200

    Añadiendo el paquete DAO

--- src/main/java/com/example/demo/domain/Persona.java
+++ src/main/java/com/example/demo/domain/Persona.java
@@ -1,8 +1,19 @@
 package com.example.demo.domain;
 
 import lombok.Data;
+import javax.persistence.*;
+import java.io.Serializable;
+
 @Data
-public class Persona {
+@Entity
+@Table(name = "persona")
+public class Persona implements Serializable {
+
+    private static final long serialVersionUId = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_persona;
     private String nombre;
     private String apellido;
     private String telefono;
